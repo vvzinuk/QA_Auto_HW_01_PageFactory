1ee9d8b81726179e2166c05064c403fe48b40bb1
vvzinuk <vvzinuk@gmail.com>
2019-04-10 23:13:07 +0300

    POM implemented

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -5,26 +5,11 @@ public class HomePage {
     By loginButton = By.linkText( "Sign in" );
 
     WebDriver driver;
-    LoginPage loginPage = new LoginPage( this.driver );
-    CategoryPage categoryWomenPage;
-
 
     public HomePage(WebDriver driver) {
         this.driver = driver;
     }
 
-    public HomePage login(){
-        System.out.println(1);
-        driver.findElement(loginButton).click();
-        System.out.println(2);
-        loginPage.typeUserName();
-        loginPage.typePasswd();
-        System.out.println(3);
-        driver.findElement( loginPage.submitButton ).click();
-        System.out.println(4);
-        driver.get( " http://automationpractice.com " );
-        return new HomePage( driver );
-    }
 
     public LoginPage openLoginPage (){
         driver.findElement(loginButton).click();
